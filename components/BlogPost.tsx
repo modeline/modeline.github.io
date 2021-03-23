ba0d700af402184fc6fe4262cbf3805b75e479c7
Tomasz Miśków <miskow.tomasz@gmail.com>
2021-03-23 15:45:02 +0100

    Add Tailwind CSS

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -9,10 +9,21 @@ interface BlogPostProps {
   meta: BlogPostMeta;
 }
 
-export const BlogPost: React.FC<BlogPostProps> = ({ meta, children }) => (
-  <div>
-    <h1>{meta.title}</h1>
-    <h2>by {meta.author}</h2>
-    {children}
+export const BlogPost: React.FC<BlogPostProps> = ({meta}) => (
+  <div className="lg:flex lg:items-center lg:justify-between">
+    <div className="flex-1 min-w-0">
+      <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+        {meta.title}
+      </h2>
+      <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+        <div className="mt-2 flex items-center text-sm text-gray-500">
+          <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"/>
+          </svg>
+          by {meta.author}
+        </div>
+      </div>
+    </div>
   </div>
 );
